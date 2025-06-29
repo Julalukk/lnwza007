2989446b310cc798092a96d880e42a1debe6211f
Julaluk <Julaluk3617@gmail.com>
2025-06-29 14:47:47 +0700

    first commit

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>จุฬาลักษณ์ แสนขัด 67222420014</h1>
+    <a href="/gallery/ant"><img src="{{$ant}}" /></a>
+    <a href="/gallery/bird"><img src="{{$bird}}" /></a>
+    <a href="/gallery/cat "><img src="{{$cat}}" /></a>
+</body> 
+</html>
